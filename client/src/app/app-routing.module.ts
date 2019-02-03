687eaf607281e3739a42ba2687fc50c7e80f8c01
Kevin Stradtman <stradtkt22@gmail.com>
2019-02-02 23:00:56 -0500

    Added a ui-kit and added routes

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { CompletedComponent } from './components/completed/completed.component';
+import { ActiveComponent } from './components/active/active.component';
 
-const routes: Routes = [];
+
+const routes: Routes = [
+  {path: '', component: TasksComponent},
+  {path: 'completed', component: CompletedComponent},
+  {path: 'active', component: ActiveComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -5,10 +5,22 @@ import { TasksService } from './tasks.service';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { SearchComponent } from './components/search/search.component';
+import { TasksComponent } from './components/tasks/tasks.component';
+import { CompletedComponent } from './components/completed/completed.component';
+import { ActiveComponent } from './components/active/active.component';
+import { AddComponent } from './components/add/add.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SearchComponent,
+    TasksComponent,
+    CompletedComponent,
+    ActiveComponent,
+    AddComponent
   ],
   imports: [
     BrowserModule,
